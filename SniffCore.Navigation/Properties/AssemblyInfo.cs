c09412ff7bbc9ecd5d6cae4010ab62bf6c35d3ad
David Wendland <the.devicenator@gmail.com>
2021-06-05 09:13:12 +0200

    Add XML NS

--- SniffCore.Navigation/Properties/AssemblyInfo.cs
+++ SniffCore.Navigation/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: ComVisible(false)]
 [assembly: Guid("62c39367-a706-4585-9d92-aff9f462ffe0")]
+[assembly: XmlnsDefinition("http://sniffcore.com", "SniffCore.Navigation")]
